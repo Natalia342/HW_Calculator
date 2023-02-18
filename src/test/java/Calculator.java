21e295e2a7ecff2b81db9957878e217eaca7cc3e
Natalia342 <nataliatalova7@gmail.com>
2023-02-19 01:11:04 +0200

    noStatic

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+public class Calculator extends JFrame {
+    int a;
+    int b;
+    char action;
+   public String res = ("0");
+
+    public static void print(){
+        System.out.println("This is a calculator");
+
+
+    }
+    public String calc(String a, String b){
+        return a+b;}
+//    public String calc(String a, String b){
+ //       return a-b;}
+
+}
